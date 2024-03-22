7b6a1481258bea68f4205dbe208181a8a4edca84
SazanamiInko <s.suzuki1614@gmail.com>
2024-03-22 20:46:23 +0900

    引き算

---
+++ Src/PG/03_ope/pg0008_ope_minus/main.go
@@ -0,0 +1,19 @@
+// ///////////////////
+// /
+// / -演算子
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+
+	var ans int
+	ans = 5 - 2
+
+	fmt.Print("5-2=")
+	fmt.Println(ans)
+
+}
