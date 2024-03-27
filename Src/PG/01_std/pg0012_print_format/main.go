59e1e527990c6bfca6b632f509307798f4c831f7
SazanamiInko <s.suzuki1614@gmail.com>
2024-03-27 19:43:02 +0900

    剰余と形式表示

---
+++ Src/PG/01_std/pg0012_print_format/main.go
@@ -0,0 +1,20 @@
+// ///////////////////
+// /
+// / 形式表示
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+
+	var ans int
+	var mod_num int
+
+	ans = 5 / 2
+	mod_num = 5 % 2
+
+	fmt.Printf("5/2=%d 余り %d", ans, mod_num)
+}
---
+++ Src/PG/03_ope/pg0011_ope_mod/main.go
@@ -0,0 +1,23 @@
+// ///////////////////
+// /
+// / %演算子
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+
+	var ans int
+	var mod_num int
+
+	ans = 5 / 2
+	mod_num = 5 % 2
+
+	fmt.Print("5/2=")
+	fmt.Println(ans)
+	fmt.Print("余り")
+	fmt.Println(mod_num)
+}
