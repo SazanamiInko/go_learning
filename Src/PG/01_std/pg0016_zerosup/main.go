758e0c4dcdf18fe40dac6148c737a3fd54e87a9d
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-09 20:35:17 +0900

    ゼロ埋め

---
+++ Src/PG/01_std/pg0016_zerosup/main.go
@@ -0,0 +1,18 @@
+// ///////////////////
+// /
+// / 0埋め表示
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+	var nisi = 2 * 4
+	var nigo = 2 * 5
+
+	fmt.Printf("にしが%02d", nisi)
+	fmt.Println("")
+	fmt.Printf("にごが%02d", nigo)
+}
