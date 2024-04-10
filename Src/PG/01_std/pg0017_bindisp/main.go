396ef10e443cfd53beaa91e49fd3c861a9bcd54e
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-10 20:52:12 +0900

    2進数表示

---
+++ Src/PG/01_std/pg0017_bindisp/main.go
@@ -0,0 +1,18 @@
+// ///////////////////
+// /
+// / ２進数表示
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+	var fourteen = 14
+	var fiveteen = 15
+
+	fmt.Printf("14={%b}", fourteen)
+	fmt.Println("")
+	fmt.Printf("15={%b}", fiveteen)
+}
