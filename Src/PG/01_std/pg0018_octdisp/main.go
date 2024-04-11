f4a1459b78c270a529a68b064075569972f0518a
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-11 22:13:33 +0900

    8進数表示

---
+++ Src/PG/01_std/pg0018_octdisp/main.go
@@ -0,0 +1,19 @@
+// ///////////////////
+// /
+// / 8進数表示
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+
+	var sixtytree = 63
+	var seventytwo = 72
+
+	fmt.Printf("63={%o}", sixtytree)
+	fmt.Println("")
+	fmt.Printf("72={%o}", seventytwo)
+}
