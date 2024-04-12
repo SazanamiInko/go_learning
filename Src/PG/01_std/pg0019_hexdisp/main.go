51532b39ccdeb168e83e1e553eb1a0e570b77f1c
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-12 20:08:04 +0900

    16進数表示

---
+++ Src/PG/01_std/pg0019_hexdisp/main.go
@@ -0,0 +1,19 @@
+// ///////////////////
+// /
+// / 16進数表示
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+
+	var twentyaeven = 27
+	var big = 253
+
+	fmt.Printf("27={%x}", twentyaeven)
+	fmt.Println("")
+	fmt.Printf("253={%x}", big)
+}
