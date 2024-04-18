f931feb65b261e22ac9b63bc35b6e2ca428cfa80
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-18 15:17:14 +0900

    文字->数値変換

---
+++ Src/work/tr0002_int_weight_meter/main.go
@@ -0,0 +1,30 @@
+// ///////////////////
+// /
+// / int体重計
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//生徒構造体
+type TStudent struct
+{
+	name string
+	weight float32
+	record int32
+}
+
+
+//メイン関数
+func main() {
+
+}
+
+///int32体重計
+///student:生徒
+///戻り値 :体重計
+func  dispWeihtMeter(student TStudent) int32
+{
+	return Math
+}
