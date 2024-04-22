5c7d3c982b1a1115f84f8ea384af0b607ae635c1
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-22 21:15:21 +0900

    bool型

---
+++ Src/PG/02_val/pg0026_boolean/main.go
@@ -0,0 +1,19 @@
+// ///////////////////
+// /
+// / bool型
+// /
+// ////////////////////
+package main
+
+import "fmt"
+
+//メイン関数
+func main() {
+	var trueVal = true
+	var falseVal = false
+
+	fmt.Printf("それでは歌っていただきましょう。\r\n")
+	fmt.Printf("白組 %t love\r\n", trueVal)
+	fmt.Printf("紅組 恋は%t\r\n", falseVal)
+
+}
