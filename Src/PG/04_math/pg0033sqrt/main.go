41cede16dd060efe6c03d51cb0c9cf46f8c792ba
SazanamiInko <s.suzuki1614@gmail.com>
2024-05-08 18:21:49 +0900

    平方根

---
+++ Src/PG/04_math/pg0033sqrt/main.go
@@ -0,0 +1,17 @@
+// ///////////////////
+// /
+// / ルート
+// /
+// ////////////////////
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// メイン関数
+func main() {
+	result := math.Sqrt(3)
+	fmt.Printf("3の平方根は: %.3fです\n", result)
+}
